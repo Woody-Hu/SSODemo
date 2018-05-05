188f74153cc3a102c1ebd1426f2710e70f477c74
Woody-Hu <812791840@qq.com>
2018-05-05 10:33:33 +0800

    2018 05 05

---
+++ SSODemo/Entity/AuthSession.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace SSODemo.Entity
+{
+    public class AuthSession
+    {
+
+        [Key]
+        [MaxLength(32)]
+        [Required]
+        public string SessionKey { get; set; }
+
+        [MaxLength(50)]
+        public string UserName { get; set; }
+
+        [MaxLength(15)]
+        [Required]
+        public string IpAddress { get; set; }
+
+        [Required]
+        public DateTime InvalidTime { get; set; }
+
+        [Required]
+        public DateTime CreateTime { get; set; }
+    }
+}
